f3ee3b1f58748ab915cfda2a4462076d36c33239
Rihil Sanghani <rihilsanghanigit@gmail.com>
2021-11-26 10:41:38 +0530

    first

--- Pr-1/Pr_1_1.php
+++ Pr-1/Pr_1_1.php
@@ -7,7 +7,7 @@
         echo "<h1>Whenever requesting web page with .php extension the server<br>
          locates the requested file and executing the script that is<br>
           specified between <? phpand ?> tag and returns output in form<br> 
-          of just HTML back to the user.</h1> "
+          of just HTML back to the user.</h1>  "
         ?>
     </body>
 </html>
